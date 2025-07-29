8fdc8ff1b70d43a6a04632cb2384043c42645cf9
Conor Newdick <conor.newdick@student.onslow.school.nz>
2025-07-30 10:11:42 +1200

    Added map to a diferent place and bug fixing

--- swen-2025-conservation-game/main.swift
+++ swen-2025-conservation-game/main.swift
@@ -7,18 +7,21 @@
 
 import Foundation
 
+//MARK: Variables and constants.
+
 let playerCharacter = "@"
 
-var map = [
-    [",", ",", ".", ","],
-    [",", ",", ".", ","],
-    [",", ",", ".", ","],
-    [",", ",", "@", ","]
-    ]
-var characterPosition = [3, 2]
+var characterPosition = [29, 65]
 var oldPlayerPosition = [3, 2]
 var underneathPlayer = "."
+var playerCorrectInput = true
 
+// MARK: Functions
+
+/// Gets a String user inout from the user
+///
+/// Parameters:  Prompt, Error message
+/// Return:  a String of the user's input
 func stringInput(prompt: String, errorMessage: String) -> String {
     while true {
         
@@ -31,44 +34,29 @@ func stringInput(prompt: String, errorMessage: String) -> String {
     }
 }
 
-func displayMovement(playerCharacter: String, oldPlayerPosition: [Int], playerPosition: [Int], underneathPlayer: String, mapScreen: inout [[String]]) {
-    mapScreen[playerPosition[0]][playerPosition[1]] = playerCharacter
-    mapScreen[oldPlayerPosition[0]][oldPlayerPosition[1]] = underneathPlayer
-}
-
-///Allow the player to move around the map
+/// Updates and prints the map.
 ///
-///Parameters:
-///the map which the user can move around
-func movement(playerCharacter: String, oldPlayerPosition: inout [Int], playerPosition: inout [Int], underneathPlayer: inout String, mapScreen: [[String]]) {
-    let userInput = stringInput(prompt: "Enter", errorMessage: "Fuck off")
-    switch userInput {
-    case "w" :
-        oldPlayerPosition = playerPosition
-        playerPosition[0] -= 1
-        underneathPlayer = mapScreen[playerPosition[0]][playerPosition[1]]
-    case "s" :
-        oldPlayerPosition = playerPosition
-        playerPosition[0] += 1
-        underneathPlayer = mapScreen[playerPosition[0]][playerPosition[1]]
-    case "d" :
-        oldPlayerPosition = playerPosition
-        playerPosition[1] -= 1
-        underneathPlayer = mapScreen[playerPosition[0]][playerPosition[1]]
-    case "a" :
-        oldPlayerPosition = playerPosition
-        playerPosition[1] += 1
-        underneathPlayer = mapScreen[playerPosition[0]][playerPosition[1]]
-    default :
-        print("fuck off")
-        return
-    }
-}
-
-while true {
-    movement(playerCharacter: playerCharacter, oldPlayerPosition: &oldPlayerPosition, playerPosition: &characterPosition, underneathPlayer: &underneathPlayer, mapScreen: map)
-    displayMovement(playerCharacter: playerCharacter, oldPlayerPosition: oldPlayerPosition, playerPosition: characterPosition, underneathPlayer: underneathPlayer, mapScreen: &map)
-    for row in map {
+/// Parameters:
+///
+///     playerCharacter: let constant allways contain an @ symbol.
+///     oldPlayerPosition: the position the user used to be at, will be used to replace the character that used to be there.
+///     playerPosition: where the player is now.
+///     underneathPlayer: the character the player is currently covering.
+///     mapscreen: the 2d array used for the screen.
+func updateMap(playerCharacter: String, oldPlayerPosition: [Int], playerPosition: [Int], underneathPlayer: inout String, mapScreen: inout [[String]]) {
+    
+    // Clear the screen
+    print("\u{001B}[2J")
+    
+    // Replace where the charcter used to be with the character that was "underneath" them.
+    mapScreen[oldPlayerPosition[0]][oldPlayerPosition[1]] = underneathPlayer
+    // Get the new character which is undertheath the player.
+    underneathPlayer = mapScreen[playerPosition[0]][playerPosition[1]]
+    // add the player to the map in the new position
+    mapScreen[playerPosition[0]][playerPosition[1]] = playerCharacter
+    
+    // Display the map
+    for row in mapScreen {
         for colum in row {
             print(colum, terminator: "")
         }
@@ -76,4 +64,45 @@ while true {
     }
 }
 
+/// Allow the player to move around the map
+///
+/// Parameters:
+///
+///     oldPlayerPosition:
+///     playerPosition:
+///     mapScreen: the map which the user can move around on.
+///     playerCorrectInput: whether the user made a valid input.
+func movement(oldPlayerPosition: inout [Int], playerPosition: inout [Int], mapScreen: [[String]], playerCorrectInput: inout Bool) {
+    let userInput = stringInput(prompt: "Enter", errorMessage: "how")
+    switch userInput {
+    case "w" :
+        oldPlayerPosition = playerPosition
+        playerPosition[0] -= 1
+    case "s" :
+        oldPlayerPosition = playerPosition
+        playerPosition[0] += 1
+    case "d" :
+        oldPlayerPosition = playerPosition
+        playerPosition[1] += 1
+    case "a" :
+        oldPlayerPosition = playerPosition
+        playerPosition[1] -= 1
+    default :
+        print("fuck off")
+        playerCorrectInput = false
+        return
+    }
+    playerCorrectInput = true
+}
 
+//MARK: Main code
+
+updateMap(playerCharacter: playerCharacter, oldPlayerPosition: oldPlayerPosition, playerPosition: characterPosition, underneathPlayer: &underneathPlayer, mapScreen: &map)
+
+while true {
+    movement(oldPlayerPosition: &oldPlayerPosition, playerPosition: &characterPosition, mapScreen: map, playerCorrectInput: &playerCorrectInput)
+    
+    if playerCorrectInput {
+        updateMap(playerCharacter: playerCharacter, oldPlayerPosition: oldPlayerPosition, playerPosition: characterPosition, underneathPlayer: &underneathPlayer, mapScreen: &map)
+    }
+}
